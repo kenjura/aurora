92db1d5829ee41914e77be2cb33b168be8117db5
Andrew London <alondon@paypal.com>
2020-01-27 14:07:33 -0800

    removed spammy logs

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ htmlEngine(app);
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
+// app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').text());
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
