d000ec7168eabd3cab0f79d8a9255f74ddbaa07c
Andrew London <purple-cat@kenjura.com>
2024-09-06 16:54:41 -0700

    added frontmatter support

--- src/helpers/markdownHelper.js
+++ src/helpers/markdownHelper.js
@@ -1,38 +1,45 @@
-const markdown = require('markdown-it')({ html:true });
+const markdown = require("markdown-it")({ html: true });
 
-markdown.use(require('markdown-it-container'), 'aside', getAside());
-markdown.use(require('markdown-it-deflist'));
-markdown.use(require('markdown-it-sup'));
+markdown.use(require("markdown-it-container"), "aside", getAside());
+markdown.use(require("markdown-it-deflist"));
+markdown.use(require("markdown-it-sup"));
+markdown.use(require("markdown-it-front-matter"), handleFrontMatter);
 
-module.exports = { markdownToHtml }
+module.exports = { markdownToHtml };
 
 function markdownToHtml(md) {
-	return markdown.render(md);
+  return markdown.render(md);
 }
 
-
-
 function getAside() {
-	return {
+  return {
+    validate: function (params) {
+      console.log(
+        "validate",
+        `params="${params}"`,
+        "match=",
+        params.trim().match(/^aside\s+(.*)$/)
+      );
+      return true;
+      return params.trim().match(/^aside\s+(.*)$/);
+    },
 
-		validate: function(params) {
-			console.log('validate', `params="${params}"`, 'match=', params.trim().match(/^aside\s+(.*)$/));
-			return true;
-			return params.trim().match(/^aside\s+(.*)$/);
-		},
+    render: function (tokens, idx) {
+      var m = tokens[idx].info.trim().match(/^aside\s+(.*)$/);
 
-		render: function (tokens, idx) {
-			var m = tokens[idx].info.trim().match(/^aside\s+(.*)$/);
+      if (tokens[idx].nesting === 1) {
+        // opening tag
+        // return '<aside><summary>ASIDE' + m[1] + '</summary>\n';
+        return "<aside>\n";
+      } else {
+        // closing tag
+        return "</aside>\n";
+      }
+    },
+  };
+}
 
-			if (tokens[idx].nesting === 1) {
-				// opening tag
-				// return '<aside><summary>ASIDE' + m[1] + '</summary>\n';
-				return '<aside>\n';
-
-			} else {
-				// closing tag
-				return '</aside>\n';
-			}
-		}
-	}
+function handleFrontMatter(data) {
+  console.log("front matter", data);
+  return "blargo";
 }
